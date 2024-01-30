8baa7cc87c1d36a7fa76df9f661af881e80221e1
Sneha Bhagat Kalwar <76205696+snehabhagat@users.noreply.github.com>
2024-01-30 23:24:19 +0530

    Time: 1890 ms (5.01%) | Memory: 52.9 MB (5.13%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,32 +1,39 @@
 class Solution {
 public:
-    int n;
-    vector<int> dp;
-    bool solve(vector<int>&nums,int idx){
-        if(idx>=n-1){
-            return true;
-        }
-        if(dp[idx]!=-1){
-            return dp[idx];
-        }
-        if(nums[idx]==0){
-            return false;
-        }
-        bool a=false,b=false;
-        int i=nums[idx];
-        while(i>0){
-            a=solve(nums,idx+i);
-            if(a){
-                return dp[idx]=a;
-            }
-            i--;
-            b=a|b;
-        }
-        return dp[idx]=b;
-    }
+    
+    // bool solve(vector<int>&nums,int idx){
+    //     if(idx>=n-1){
+    //         return true;
+    //     }
+    //     if(dp[idx]!=-1){
+    //         return dp[idx];
+    //     }
+    //     if(nums[idx]==0){
+    //         return false;
+    //     }
+    //     bool a=false,b=false;
+    //     int i=nums[idx];
+    //     while(i>0){
+    //         a=solve(nums,idx+i);
+    //         if(a){
+    //             return dp[idx]=a;
+    //         }
+    //         i--;
+    //         b=a|b;
+    //     }
+    //     return dp[idx]=b;
+    // }
     bool canJump(vector<int>& nums) {
-        n=nums.size();
-        dp.resize(n,-1);
-        return solve(nums,0);
+        int n=nums.size();
+        vector<int> dp(n);
+        dp[n-1]=1;
+        for(int idx=n-1;idx>=0;idx--){
+            for(int i=1;i<=nums[idx];i++){
+                if(idx+i<n && dp[idx+i]){
+                    dp[idx]=true;
+                }
+            }
+        }
+        return dp[0];
     }
 };
