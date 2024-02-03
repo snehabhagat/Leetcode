5934c37d71ef4c72d21b0baab63d66a42e422abe
Sneha Bhagat Kalwar <76205696+snehabhagat@users.noreply.github.com>
2024-02-04 00:16:31 +0530

    Time: 1646 ms (20.00%) | Memory: 191.1 MB (40.00%) - LeetSync

---
+++ 3265-maximum-good-subarray-sum/maximum-good-subarray-sum.cpp
@@ -0,0 +1,42 @@
+class Solution {
+    #define ll long long
+public:
+    // x = k+y
+    // x = y-k
+
+    // abs (x-y) = k 
+  long long maximumSubarraySum(vector<int>& nums, int k) {
+        int n = nums.size();
+        long long ans = LLONG_MIN; 
+
+        vector<long long> prefixSum(1, 0);
+
+        unordered_map<int, vector<int>> mp;  
+        
+        for (int i = 0; i < n; i++) {
+            prefixSum.push_back(nums[i] + prefixSum[i]);
+
+            // ll a = k + nums[i];
+            // ll b = nums[i] - k;
+
+            if (mp.find(k + nums[i]) != mp.end()) {
+               //vector<int>temp = mp[k + nums[i]];
+                for(auto idx: mp[k + nums[i]]){
+                    long long tem = prefixSum[i+1] - prefixSum[idx];
+                    ans = max(ans, tem);
+                }
+            }
+            if (mp.find(nums[i] - k) != mp.end()) {
+
+                 //vector<int>temp = mp[nums[i] - k];
+                 for(auto idx: mp[nums[i] - k]){
+                    long long tem = prefixSum[i+1] - prefixSum[idx];
+                    ans = max(ans, tem);
+                 }
+            }
+            mp[nums[i]].push_back(i);  
+        }
+
+        return ans == LLONG_MIN ? 0 : ans;  
+  }
+};
