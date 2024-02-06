d09cd2021b88b530b452372a76da749b6de2f5c0
Sneha Bhagat Kalwar <76205696+snehabhagat@users.noreply.github.com>
2024-02-06 19:38:26 +0530

    Time: 35 ms (25.32%) | Memory: 23.3 MB (5.23%) - LeetSync

--- 1503-reducing-dishes/reducing-dishes.cpp
+++ 1503-reducing-dishes/reducing-dishes.cpp
@@ -2,29 +2,21 @@ class Solution {
 public:
     int n;
     vector<vector<int>> dp;
-    int solve(vector<int>&sat,int idx,int mul){
-        if(idx==n){
+    int solve(vector<int>&sat,int idx,int time){
+        if(idx>=n){
             return 0;
         }
-        if(dp[idx][mul]!=-1){
-            return dp[idx][mul];
+        if(dp[idx][time]!=INT_MIN){
+            return dp[idx][time];
         }
-        int take=0,not_take=0;
-        take=sat[idx]*mul+solve(sat,idx+1,mul+1);
-        not_take=solve(sat,idx+1,mul);
-        return dp[idx][mul]=max(take,not_take);
+        int take=sat[idx]*time+solve(sat,idx+1,time+1);
+        int not_take=solve(sat,idx+1,time);
+        return dp[idx][time]=max(take,not_take);
     }
     int maxSatisfaction(vector<int>& sat) {
         n=sat.size();
-        dp.resize(n+1,vector<int>(n+2,0));
         sort(sat.begin(),sat.end());
-        //return solve(sat,0,1);
-        for(int i=n-1;i>=0;i--){
-            for(int j=1;j<=n;j++){
-                int x=sat[i]*j+dp[i+1][j+1];
-                dp[i][j]=max(x,dp[i+1][j]);
-            }
-        }
-        return dp[0][1];
+        dp.resize(n+1,vector<int>(n+1,INT_MIN));
+        return solve(sat,0,1);
     }
 };
