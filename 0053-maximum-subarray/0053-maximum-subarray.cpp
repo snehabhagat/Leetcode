1bdab6184f7e601d83741eb05c4b7f2334f36669
Sneha Bhagat Kalwar <76205696+snehabhagat@users.noreply.github.com>
2024-06-10 23:33:45 +0530

    Time: 109 ms (5.03%), Space: 71.2 MB (7.62%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,15 +1,31 @@
 class Solution {
 public:
+    int merge(vector<int> &nums,int left,int mid,int right){
+        int leftsum=INT_MIN,rightsum=INT_MIN;
+        int s=0;
+        for(int i=mid;i>=left;i--){
+            s+=nums[i];
+            leftsum=max(leftsum,s);
+        }
+        s=0;
+        for(int i=mid+1;i<=right;i++){
+            s+=nums[i];
+            rightsum=max(rightsum,s);
+        }
+        return leftsum+rightsum;
+    }
+    int solve(vector<int> &nums,int left,int right){
+        if(left==right){
+            return nums[left];
+        }
+        int mid=left+(right-left)/2;
+        int left_sum=solve(nums,left,mid);
+        int right_sum=solve(nums,mid+1,right);
+        int sum=merge(nums,left,mid,right);
+        return max({sum,left_sum,right_sum});
+    }
     int maxSubArray(vector<int>& nums) {
         int n=nums.size();
-        int max_till=INT_MIN,ans=INT_MIN,sum_till=0;
-        for(int i=0;i<n;i++){
-            if(sum_till<0){
-                sum_till=0;
-            }
-            sum_till+=nums[i];
-            ans=max(ans,sum_till);
-        }
-        return ans;
+        return solve(nums,0,n-1);
     }
 };
